97c289a69236dcde16bcce630899e4d1ec97146e
Andrew M. Choe <lisa.su@kakao.com>
2022-06-08 23:59:37 +0900

    Update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.10"
     id("org.jetbrains.dokka") version "1.6.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.30"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
     id("idea")
     id("eclipse")
     application
@@ -23,7 +23,7 @@ repositories {
 val exposedVersion: String by project
 val ktorVersion = "1.6.7"
 dependencies {
-    implementation("io.netty:netty-all:4.1.75.Final")
+    implementation("io.netty:netty-all:4.1.77.Final")
     implementation("mysql", "mysql-connector-java", "8.0.25")
     implementation("org.graalvm.js", "js-scriptengine", "21.2.0")
     implementation("org.graalvm.js", "js", "21.2.0")
@@ -34,23 +34,23 @@ dependencies {
     implementation("io.github.microutils", "kotlin-logging", "2.0.11")
     //implementation("org.slf4j:slf4j-simple:1.7.26")
     implementation("com.microsoft.sqlserver", "mssql-jdbc", "9.2.1.jre11")
-    implementation("org.postgresql:postgresql:42.3.3")
+    implementation("org.postgresql:postgresql:42.3.4")
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
-    implementation("com.h2database:h2:2.1.210")
+    implementation("com.h2database:h2:2.1.212")
     implementation("com.beust", "klaxon", "5.5")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-css:1.0.0-pre.236-kotlin-1.5.30")
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.3")
-    implementation("io.ktor:ktor-server-content-negotiation:2.0.0-eap-256")
-    implementation("io.ktor:ktor-server-auth:2.0.0-eap-256")
-    implementation("io.ktor:ktor-server-core-jvm:2.0.0-eap-256")
-    implementation("io.ktor:ktor-server-netty-jvm:2.0.0-eap-256")
-    implementation("io.ktor:ktor-server-html-builder-jvm:2.0.0-eap-256")
-    implementation("io.ktor:ktor-server-sessions-jvm:2.0.0-eap-256")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.0.0-eap-256")
-    dokkaGfmPlugin("org.jetbrains.dokka:jekyll-plugin:1.6.10")
-    dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.10")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.4")
+    implementation("io.ktor:ktor-server-content-negotiation:2.0.1")
+    implementation("io.ktor:ktor-server-auth:2.0.1")
+    implementation("io.ktor:ktor-server-core-jvm:2.0.1")
+    implementation("io.ktor:ktor-server-netty-jvm:2.0.1")
+    implementation("io.ktor:ktor-server-html-builder-jvm:2.0.1")
+    implementation("io.ktor:ktor-server-sessions-jvm:2.0.1")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.0.1")
+    dokkaGfmPlugin("org.jetbrains.dokka:jekyll-plugin:1.6.20")
+    dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.6.20")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.10")
 }
 
