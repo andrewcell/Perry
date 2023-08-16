936f711aaebd726ecbba6d6c48cc665de79a9efb
Seungyeon Choi <git@vxz.me>
2023-08-16 18:27:56 +0900

    Update dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("org.jetbrains.dokka") version "1.7.20"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.dokka") version "1.8.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id("idea")
     id("eclipse")
     application
@@ -20,24 +20,24 @@ val exposedVersion: String by project
 val ktorVersion: String by project
 
 dependencies {
-    implementation("io.netty:netty-all:4.1.86.Final")
-    implementation("mysql", "mysql-connector-java", "8.0.31")
-    implementation("org.graalvm.js", "js-scriptengine", "22.3.0")
-    implementation("org.graalvm.js", "js", "22.3.0")
+    implementation("io.netty:netty-all:4.1.96.Final")
+    implementation("mysql", "mysql-connector-j", "8.0.33")
+    implementation("org.graalvm.js", "js-scriptengine", "23.0.1")
+    implementation("org.graalvm.js", "js", "23.0.1")
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-java-time", exposedVersion)
-    implementation("io.github.microutils", "kotlin-logging", "3.0.4")
+    implementation("io.github.microutils", "kotlin-logging", "3.0.5")
     //implementation("org.slf4j:slf4j-simple:1.7.26")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:12.1.0.jre11-preview")
-    implementation("org.postgresql:postgresql:42.5.1")
-    implementation("org.xerial:sqlite-jdbc:3.40.0.0")
-    implementation("com.h2database:h2:2.1.214")
+    implementation("com.microsoft.sqlserver:mssql-jdbc:12.4.0.jre11")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.xerial:sqlite-jdbc:3.42.0.0")
+    implementation("com.h2database:h2:2.2.220")
     implementation("com.beust", "klaxon", "5.6")
-    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
     implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.4")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
@@ -48,8 +48,8 @@ dependencies {
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
-    dokkaGfmPlugin("org.jetbrains.dokka:jekyll-plugin:1.7.20")
-    dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.20")
+    dokkaGfmPlugin("org.jetbrains.dokka:jekyll-plugin:1.8.20")
+    dokkaGfmPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.8.20")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.0")
 }
 
