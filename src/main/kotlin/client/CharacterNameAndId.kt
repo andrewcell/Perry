51a23334646b6ba597398a06a2192ac80248449b
Seungyeon Choi <git@vxz.me>
2026-01-22 22:46:43 +0900

    Add comment on CharacterNameAndId

--- src/main/kotlin/client/CharacterNameAndId.kt
+++ src/main/kotlin/client/CharacterNameAndId.kt
@@ -1,3 +1,8 @@
 package client
 
+/**
+ * A class representing a character with an ID and a name.
+ * @param id The unique identifier of the character.
+ * @param name The name of the character.
+ */
 open class CharacterNameAndId(val id: Int, val name: String)
